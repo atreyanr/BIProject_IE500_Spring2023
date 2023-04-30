9b2a724c3e038a8bcc0a3068d076601b35581ed6
Atreyan Raman <124120078+atreyanr@users.noreply.github.com>
2023-04-30 17:05:02 -0400

    Major fixes across both test and train data code snippets

--- proj.R
+++ proj.R
@@ -1,3 +1,13 @@
+# Importing libraries
+
+library(caret)
+library(caTools)
+library(Metrics)
+library(MLmetrics)
+library(ModelMetrics)
+library(pROC)
+
+
 # Importing data
 
 Invistico_Airline <- read.csv("~/Library/CloudStorage/Box-Box/UB/Spring 2023/IE500 BI/Project/Invistico_Airline.csv")
@@ -16,6 +26,7 @@ Invistico_Airline$Type.of.Travel <- ifelse(Invistico_Airline$Type.of.Travel == "
 Invistico_Airline$Customer.Type <- ifelse(Invistico_Airline$Customer.Type == "Loyal Customer", 1, 0) #1 for Loyal Customer, 0 for disloyal Customer
 Invistico_Airline$satisfaction <- ifelse(Invistico_Airline$satisfaction == "satisfied", 1, 0) #1 for satisfied, 0 for dissatisfied
 
+
 # Cleaning the data and completing the empty rows so there is no mismatch.
 
 df_air <- na.omit(Invistico_Airline)
@@ -30,23 +41,27 @@ split <- caret::createDataPartition(df_air_complete$satisfaction, p=0.75, list=F
 train_df_air <- df_air_complete[split,]
 test_df_air <- df_air_complete[-split,]
 
-sample <- sample.split(df_air_complete$satisfaction, SplitRatio = 0.75)
-train_df_air <- subset(df_air_complete, sample == T)
-test_df_air <- subset(df_air_complete,sample == F)
-
 # Fitting data with logistic regression model **before** performing PCA,
 # first on train data and then on test data
 
 # For training dataset
 log_reg_train <- glm(satisfaction ~ ., data = train_df_air, family = binomial(link = "logit"))
 
-pred_train <- predict(log_reg_train, type = "response")
+summary(log_reg_train)
+
+pred_train <- predict(log_reg_train, newdata = train_df_air, type = "response")
+
 train_preds <- ifelse(pred_train > 0.5, "satisfied","dissatisfied")
 train_actuals <- ifelse(train_df_air$satisfaction == 1, "satisfied","dissatisfied")
 
-# Compute confusion matrix
-train_cm <- confusionMatrix(train_preds, train_actuals, positive = "satisfied")
+train_actuals <- factor(train_actuals, levels = c("dissatisfied", "satisfied"))
+train_preds <- factor(train_preds, levels = c("dissatisfied", "satisfied"))
 
+
+# Compute confusion matrix
+
+train_cm <- confusionMatrix(train_preds, train_actuals, positive = "satisfied")
+train_cm
 
 train_cm_table <- table(train_actuals, train_preds)
 
@@ -65,13 +80,17 @@ if ("satisfied" %in% rownames(train_cm_table) && "satisfied" %in% colnames(train
 }
 
 # Compute F1 score
-train_f1 <- f1(train_cm_table, posPredValue = "satisfied", negPredValue = "dissatisfied")
+train_f1 <- F1_Score(train_preds, train_actuals, positive = "satisfied")
+
+# Convert actuals to numeric
+train_actuals_numeric <- as.numeric(train_actuals) - 1
+train_preds_numeric <- as.numeric(train_preds) - 1
 
 # Compute AUC-ROC
-train_auc <- caret::roc(train_actuals, pred_train)$auc
+train_auc <- AUC(train_preds,train_actuals_numeric)
 
 # Compute Brier score
-train_brier <- caret::BrierScore(train_preds, train_actuals, positive = "satisfied")
+train_brier <- brier(train_preds, train_actuals)
 
 cat("Train Recall: ", train_recall, "\n")
 cat("Train Precision: ", train_precision, "\n")
@@ -82,12 +101,21 @@ cat("Train Brier Score:", train_brier, "\n")
 # For testing dataset
 log_reg_test <- glm(satisfaction ~ ., data = test_df_air, family = binomial(link = "logit"))
 
+summary(log_reg_test)
+
 pred_test <- predict(log_reg_test, type = "response")
 test_preds <- ifelse(pred_test > 0.5, "satisfied","dissatisfied")
 test_actuals <- ifelse(test_df_air$satisfaction == 1, "satisfied","dissatisfied")
 
+
+test_actuals <- factor(test_actuals, levels = c("dissatisfied", "satisfied"))
+test_preds <- factor(test_preds, levels = c("dissatisfied", "satisfied"))
+
+
 # Compute confusion matrix
+
 test_cm <- confusionMatrix(test_preds, test_actuals, positive = "satisfied")
+test_cm
 
 
 test_cm_table <- table(test_actuals, test_preds)
@@ -106,16 +134,24 @@ if ("satisfied" %in% rownames(test_cm_table) && "satisfied" %in% colnames(test_c
   cat("No positive cases in actuals or predicted values.")
 }
 
+# Compute F1 score
+test_f1 <- F1_Score(test_preds, test_actuals, positive = "satisfied")
+
+# Convert actuals to numeric
+test_actuals_numeric <- as.numeric(test_actuals) - 1
+test_preds_numeric <- as.numeric(test_preds) - 1
+
+# Compute AUC-ROC
+test_auc <- AUC(test_preds,test_actuals_numeric)
+
+
+# Compute Brier score
+test_brier <- brier(test_preds, test_actuals)
+
+
 cat("Test Recall: ", test_recall, "\n")
 cat("Test Precision: ", test_precision, "\n")
+cat("Test F1 Score:", test_f1, "\n")
+cat("Test AUC-ROC:", test_auc, "\n")
+cat("Test Brier Score:", test_brier, "\n")
 
-
-
-
-
-# Compute F1-score, AUC-ROC, and Brier score
-test_summary <- twoClassSummary(test_preds, test_actuals, positive = "satisfied")
-
-test_f1 <- test_summary["F1"]
-test_auc <- test_summary["ROC"]
-test_brier <- test_summary["Brier"]
